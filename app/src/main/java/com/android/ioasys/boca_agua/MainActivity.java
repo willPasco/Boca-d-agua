6c48cc717d56ce230ce63f474b27a57c220c0951
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-03-26 12:17:26 -0300

    Pushing app.

---
+++ app/src/main/java/com/android/ioasys/boca_agua/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.ioasys.boca_agua;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
