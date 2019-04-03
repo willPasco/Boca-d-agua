e60839da9f532d5d5fdd36e7689c7438c1414450
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-04-03 17:25:26 -0300

    Implementing the activity to show all recipes.

--- app/src/main/java/com/android/ioasys/boca_agua/MainActivity.java
+++ app/src/main/java/com/android/ioasys/boca_agua/MainActivity.java
@@ -1,8 +1,12 @@
 package com.android.ioasys.boca_agua;
 
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import com.android.ioasys.boca_agua.viewmodel.InitialRequestViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
         InitialRequestViewModel viewModel = ViewModelProviders.of(this).get(InitialRequestViewModel.class);
 
-        viewModel.loadInitialContent();
+        final MutableLiveData<Integer> requestCode = viewModel.loadInitialContent();
+
+        requestCode.observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(@Nullable Integer code) {
+                Log.i("Test", code+"");
+                if (code == 200) {
+                    RecipeListActivity_.intent(MainActivity.this).start();
+                }
+            }
+        });
     }
 }
