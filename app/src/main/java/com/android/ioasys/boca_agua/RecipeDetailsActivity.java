fa8fff468c36c09d33796486f40360b8670a9a6d
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-04-03 19:15:53 -0300

    Implementing Recipe Details activity.

---
+++ app/src/main/java/com/android/ioasys/boca_agua/RecipeDetailsActivity.java
@@ -0,0 +1,89 @@
+package com.android.ioasys.boca_agua;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.ioasys.boca_agua.model.Recipe;
+import com.android.ioasys.boca_agua.model.RecipeWithElements;
+import com.android.ioasys.boca_agua.viewmodel.RecipeViewModel;
+import com.bumptech.glide.Glide;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_recipe_details)
+public class RecipeDetailsActivity extends AppCompatActivity {
+
+    @ViewById(R.id.image_view_recipe_image)
+    ImageView recipeImage;
+
+    @ViewById(R.id.text_view_recipe_name)
+    TextView recipeName;
+
+    @ViewById(R.id.text_view_recipe_serving)
+    TextView recipeServings;
+
+    @ViewById(R.id.recycler_view_ingredients)
+    RecyclerView recyclerViewIngredients;
+
+    @ViewById(R.id.recycler_view_steps)
+    RecyclerView recyclerViewSteps;
+
+    @Extra
+    int recipeId;
+
+    @AfterViews
+    void afterViews(){
+        RecipeViewModel viewModel = ViewModelProviders.of(this).get(RecipeViewModel.class);
+        CustomLinearLayoutManager linearLayoutManager = new CustomLinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        final IngredientsRecyclerAdapter adapterIngredient = new IngredientsRecyclerAdapter();
+        recyclerViewIngredients.setLayoutManager(linearLayoutManager);
+        recyclerViewIngredients.setAdapter(adapterIngredient);
+
+        linearLayoutManager = new CustomLinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        
+        final StepsRecyclerAdapter adapterStep = new StepsRecyclerAdapter();
+        recyclerViewSteps.setLayoutManager(linearLayoutManager);
+        recyclerViewSteps.setAdapter(adapterStep);
+
+        viewModel.getRecipeWithElements(recipeId).observe(this, new Observer<RecipeWithElements>() {
+            @Override
+            public void onChanged(@Nullable RecipeWithElements recipeWithElements) {
+                Recipe recipe = recipeWithElements.getRecipe();
+
+                Glide.with(RecipeDetailsActivity.this).load(recipe.getImage()).placeholder(R.drawable.ic_launcher_background).into(recipeImage);
+
+                recipeName.setText(recipe.getName());
+                recipeServings.setText(recipe.getServings()+"");
+
+                adapterIngredient.insertAll(recipeWithElements.getIngredientList());
+                adapterStep.insertAll(recipeWithElements.getStepsList());
+
+            }
+        });
+
+    }
+
+
+    private class CustomLinearLayoutManager extends LinearLayoutManager{
+
+        public CustomLinearLayoutManager(Context context, int orientation, boolean reverseLayout) {
+            super(context, orientation, reverseLayout);
+        }
+
+        @Override
+        public boolean canScrollVertically() {
+            return false;
+        }
+    }
+}
